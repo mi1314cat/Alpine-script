5e533cedd6499f6a209d6eba070856d535372e2a
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-26 15:20:20 +0800

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -11,9 +11,12 @@ apk add update
 apk add wget bash curl sudo
 read -p "按回车继续执行第一个脚本..."
 clear
+curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
 
-bash <(curl -fsSL https://github.com/mi1314cat/sing-box-xary-hysteria2.catmi/raw/refs/heads/main/alpine-hysteria2.sh)
+
 # 添加回车等待
 read -p "按回车继续执行第二个脚本..."
 
-curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
+bash <(curl -fsSL https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/alpine-hysteria2.sh)
+read -p "按回车继续执行第三个脚本..."
+bash <(curl -fsSL https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/Alpine-xray.sh)
