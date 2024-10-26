dce8e67e0b633286b116b3e0999b03785651d579
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-26 19:42:00 +0800

    Update allxray.sh

--- allxray.sh
+++ allxray.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
-# 介绍信息
-printf "\e[92m"
-printf "                       |\\__/,|   (\\\\ \n"
-printf "                     _.|o o  |_   ) )\n"
-printf "       -------------(((---(((-------------------\n"
-printf "                    alpine-catmi \n"
-printf "       -----------------------------------------\n"
-printf "\e[0m"
+
+# 定义颜色代码
 RED="\033[31m"
 GREEN="\033[32m"
 YELLOW="\033[33m"
 
-red() {
-    printf "\033[31m\033[01m$1\033[0m\n"
+# 定义彩色输出函数
+print_color() {
+    printf "\033[3%sm$1\033[0m\n" "$2"
 }
 
-green() {
-    printf "\033[32m\033[01m$1\033[0m\n"
+print_red() {
+    print_color "red" "$1"
 }
 
-yellow() {
-    printf "\033[33m\033[01m$1\033[0m\n"
+print_green() {
+    print_color "green" "$1"
 }
 
+print_yellow() {
+    print_color "yellow" "$1"
+}
+
+# 定义错误输出函数
 print_error() {
-    red "$1"
+    print_red "错误：$1"
 }
 
 # 获取当前架构
 CORE_ARCH=$(uname -m)
 
+# 安装 Xray
 install_xray() {
     # 获取最新版本号
     VERSION=$(curl -s "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
@@ -56,7 +57,7 @@ install_xray() {
     esac
 
     # 下载并解压 Xray
-    echo "正在下载 Xray 版本 ${VERSION}..."
+    print_yellow "正在下载 Xray 版本 ${VERSION}..."
     curl -L "https://github.com/XTLS/Xray-core/releases/download/${VERSION}/Xray-linux-${ARCH}.zip" -o Xray-linux-${ARCH}.zip || { print_error "下载 Xray 失败"; exit 1; }
     unzip Xray-linux-${ARCH}.zip || { print_error "解压 Xray 失败"; exit 1; }
     mv xray /usr/local/bin/xrayR || { print_error "移动文件失败"; exit 1; }
@@ -68,6 +69,7 @@ install_xray() {
 # 执行安装函数
 install_xray
 apk add iproute2
+
 # 创建 openrc 服务文件
 cat <<EOF >/etc/init.d/xrayR
 #!/sbin/openrc-run
@@ -100,6 +102,7 @@ stop() {
 EOF
 chmod +x /etc/init.d/xrayR
 
+# 随机生成域名
 random_website() {
     domains=(
         "one-piece.com"
@@ -138,172 +141,149 @@ random_website() {
 
     total_domains=${#domains[@]}
     random_index=$((RANDOM % total_domains))
-    
-    # 输出选择的域名
     echo "${domains[$random_index]}"
 }
 
 # 确保配置目录存在
 mkdir -p /root/Xray
 
-# 端口号输入及验证
-read -p "请输入reality端口号：" port
-sign=false
-until $sign; do
-    if [ -z "$port" ]; then
-        red "错误：端口号不能为空，请输入可用端口号!"
-        read -p "请重新输入reality端口号：" port
-        continue
-    fi
-    if ! echo "$port" | grep -qE '^[0-9]+$';then
-        red "错误：端口号必须是数字!"
-        read -p "请重新输入reality端口号：" port
-        continue
-    fi
-    if [ "$port" -lt 1 ] || [ "$port" -gt 65535 ]; then
-        red "错误：端口号必须介于1~65535之间!"
-        read -p "请重新输入reality端口号：" port
-        continue
-    fi
-    if ! nc -z 127.0.0.1 "$port" 2>/dev/null; then
-        green "成功：端口号 $port 可用!"
-        sign=true
-    else
-        red "错误：$port 已被占用！"
-        read -p "请重新输入reality端口号：" port
-    fi
-done
-generate_port() {
-    local protocol="$1"
+# 提示输入并验证端口号
+read_and_validate_port() {
+    read -p "请输入 $1 端口号：" port
+    while ! echo "$port" | grep -qE '^[0-9]+$'; do
+        print_error "错误：端口号必须是数字!"
+        read -p "请重新输入 $1 端口号：" port
+    done
+    while [ "$port" -lt 1 ] || [ "$port" -gt 65535 ]; do
+        print_error "错误：端口号必须介于1~65535之间!"
+        read -p "请重新输入 $1 端口号：" port
+    done
+    while ! nc -z 127.0.0.1 "$port" 2>/dev/null; do
+        print_error "错误：$port 已被占用！"
+        read -p "请重新输入 $1 端口号：" port
+    done
+    print_green "成功：端口号 $port 可用!"
+}
+
+read_and_validate_port "reality"
+
+# 生成随机端口
+generate_random_port() {
     while :; do
         port=$((RANDOM % 10001 + 10000))
-        read -p "请为 ${protocol} 输入监听端口(默认为随机生成): " user_input
+        read -p "请为 $1 输入监听端口(默认为随机生成): " user_input
         port=${user_input:-$port}
         if ! ss -tuln | grep -q ":$port\b"; then
             echo "$port"
-            return 0
-        else
-            echo "端口 $port 被占用，请输入其他端口"
+            return
         fi
+        print_error "端口 $port 被占用，请输入其他端口"
     done
 }
-# 随机生成 UUID
-generate_uuid() {
-    cat /proc/sys/kernel/random/uuid
-}
 
-# 随机生成 WS 路径
-generate_ws_path() {
-    echo "/$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 10)"
-}
-# SOCKS 配置
-DEFAULT_SOCKS_USERNAME="userb"
-DEFAULT_SOCKS_PASSWORD="passwordb"
-read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
-SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
-read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
-SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
+SOCKS_PORT=$(generate_random_port "socks5")
+VMES_PORT=$(generate_random_port "vmess")
 
-# 提示输入监听端口号
-
-SOCKS_PORT=$(generate_port "socks5")
-VMES_PORT=$(generate_port "vmess")
-# 提示输入监听端口号
-
-SOCKS_PORT=$(generate_port "socks5")
-VMES_PORT=$(generate_port "vmess")
 # 生成 UUID 和密钥
 UUID=$(cat /proc/sys/kernel/random/uuid)
+
+# 提示输入回落域名
 read -rp "请输入回落域名: " dest_server
 [ -z "$dest_server" ] && dest_server=$(random_website)
+
+# 生成随机 ID
 short_id=$(dd bs=4 count=2 if=/dev/urandom | xxd -p -c 8)
+
+# 生成密钥
 keys=$(/usr/local/bin/xrayR x25519)
 private_key=$(echo "$keys" | awk '{print $3}')
 public_key=$(echo "$keys" | awk '{print $6}')
-green "private_key: $private_key"
-green "public_key: $public_key"
-green "short_id: $short_id"
+
+# 输出密钥和 ID
+print_green "private_key: $private_key"
+print_green "public_key: $public_key"
+print_green "short_id: $short_id"
 
 # 生成 Xray 配置文件
 rm -f /root/Xray/config.json
 cat << EOF > /root/Xray/config.json
 {
   "inbounds": [
-  {
-            "port": ${VMES_PORT},
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "${UUID}",
-                        "alterId": 64
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "ws",
-                "wsSettings": {
-                    "path": "${WS_PATH}"
-                }
-            }
-        },
-        {
-            "listen": "::",
-            "port": ${SOCKS_PORT},
-            "protocol": "socks",
-            "settings": {
-                "auth": "password",
-                "accounts": [
-                    {
-                        "user": "${SOCKS_USERNAME}",
-                        "pass": "${SOCKS_PASSWORD}"
-                    }
-                ]
-            }
-        },
-      {
-          "listen": "0.0.0.0",
-          "port": $port,
-          "protocol": "vless",
-          "settings": {
-              "clients": [
-                  {
-                      "id": "$UUID",
-                      "flow": "xtls-rprx-vision"
-                  }
-              ],
-              "decryption": "none"
-          },
-          "streamSettings": {
-              "network": "tcp",
-              "security": "reality",
-              "realitySettings": {
-                  "show": true,
-                  "dest": "$dest_server:443",
-                  "xver": 0,
-                  "serverNames": [
-                      "$dest_server"
-                  ],
-                  "privateKey": "$private_key",
-                  "minClientVer": "",
-                  "maxClientVer": "",
-                  "maxTimeDiff": 0,
-                  "shortIds": [
-                  "$short_id"
-                  ]
-              }
+    {
+      "port": ${VMES_PORT},
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "${UUID}",
+            "alterId": 64
           }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "${WS_PATH}"
+        }
       }
+    },
+    {
+      "listen": "::",
+      "port": ${SOCKS_PORT},
+      "protocol": "socks",
+      "settings": {
+        "auth": "password",
+        "accounts": [
+          {
+            "user": "${SOCKS_USERNAME}",
+            "pass": "${SOCKS_PASSWORD}"
+          }
+        ]
+      }
+    },
+    {
+      "listen": "0.0.0.0",
+      "port": $port,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "flow": "xtls-rprx-vision"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "security": "reality",
+        "realitySettings": {
+          "show": true,
+          "dest": "$dest_server:443",
+          "xver": 0,
+          "serverNames": [
+            "$dest_server"
+          ],
+          "privateKey": "$private_key",
+          "minClientVer": "",
+          "maxClientVer": "",
+          "maxTimeDiff": 0,
+          "shortIds": [
+            "$short_id"
+          ]
+        }
+      }
+    }
   ],
   "outbounds": [
-      {
-          "protocol": "freedom",
-          "tag": "direct"
-      },
-      {
-          "protocol": "blackhole",
-          "tag": "blocked"
-      }
+    {
+      "protocol": "freedom",
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "tag": "blocked"
+    }
   ],
   "policy": {
     "handshake": 4,
@@ -319,7 +299,7 @@ EOF
 
 # 获取外部 IP
 IP=$(wget -qO- --no-check-certificate -U Mozilla https://api.ip.sb/geoip | sed -n 's/.*"ip": *"\([^"]*\).*/\1/p')
-green "您的IP为：$IP"
+print_green "您的IP为：$IP"
 
 # 生成分享链接
 share_link="vless://$UUID@$IP:$port?encryption=none&flow=xtls-rprx-vision&security=reality&sni=$dest_server&fp=chrome&pbk=$public_key&sid=$short_id&type=tcp&headerType=none#Reality"
@@ -371,10 +351,32 @@ proxies:
     udp: true
     # 这里可以添加其他的设置...
 EOF
+# 保存信息到文件
+OUTPUT_DIR="/root/xray"
+mkdir -p "$OUTPUT_DIR"
+{
+    echo "xray 安装完成！"
+    echo "服务器地址：${PUBLIC_IP}"
+    echo "vmess 端口：${VMES_PORT}"
+    echo "vmess UUID：${UUID}"
+    echo "vmess WS 路径：${WS_PATH}"
+    echo "socks5 端口：${SOCKS_PORT}"
+    echo "socks5 账号：${SOCKS_USERNAME}"
+    echo "socks5 密码：${SOCKS_PASSWORD}"
+    echo "配置文件已保存到：/root/xray/xrayS.txt"
+} > "$OUTPUT_DIR/xrayS.txt"
 
+print_info "xray 安装完成！"
+print_info "服务器地址：${PUBLIC_IP}"
+print_info "vmess 端口：${VMES_PORT}"
+print_info "vmess UUID：${UUID}"
+print_info "vmess WS 路径：${WS_PATH}"
+print_info "socks5 端口：${SOCKS_PORT}"
+print_info "socks5 账号：${SOCKS_USERNAME}"
+print_info "socks5 密码：${SOCKS_PASSWORD}"
+print_info "配置文件已保存到：/root/xray"
 # 启动服务
 rc-update add xrayR default
 service xrayR start
 
-green "安装完成！"
-green "分享链接已保存至 /root/Xray/share-link.txt"
+print_green "安装完成！"
