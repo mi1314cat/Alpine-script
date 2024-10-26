33668ebe1c763240c3e82dc7d19ace8e226392b1
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-26 14:36:48 +0800

    Update Alpine-xray.sh

--- Alpine-xray.sh
+++ Alpine-xray.sh
@@ -58,12 +58,29 @@ install_xray() {
         exit 1
     fi
 
+    # 设置适合架构的下载链接
+    case $CORE_ARCH in
+        x86_64)
+            ARCH="64"
+            ;;
+        aarch64)
+            ARCH="arm64-v8a"
+            ;;
+        armv7l)
+            ARCH="armv7a"
+            ;;
+        *)
+            print_error "不支持的架构: ${CORE_ARCH}"
+            exit 1
+            ;;
+    esac
+
     # 下载并解压 Xray
     echo "正在下载 Xray 版本 ${VERSION}..."
-    curl -L "https://github.com/XTLS/Xray-core/releases/download/${VERSION}/Xray-linux-64.zip" -o Xray-linux-64.zip || { print_error "下载 Xray 失败"; exit 1; }
-    unzip Xray-linux-64.zip || { print_error "解压 Xray 失败"; exit 1; }
+    curl -L "https://github.com/XTLS/Xray-core/releases/download/${VERSION}/Xray-linux-${ARCH}.zip" -o Xray-linux-${ARCH}.zip || { print_error "下载 Xray 失败"; exit 1; }
+    unzip Xray-linux-${ARCH}.zip || { print_error "解压 Xray 失败"; exit 1; }
     mv xray /usr/local/bin/xrayS || { print_error "移动文件失败"; exit 1; }
-    rm -f Xray-linux-64.zip  # 清理下载的 zip 文件
+    rm -f Xray-linux-${ARCH}.zip  # 清理下载的 zip 文件
 
     chmod +x /usr/local/bin/xrayS || { print_error "修改权限失败"; exit 1; }
 }
