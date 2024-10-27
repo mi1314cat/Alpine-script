55a796a16545ab46f0fbc9032ea6de75b2592368
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-27 15:19:37 +0800

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -10,15 +10,15 @@ printf "\e[0m"
 apk add update
 apk add iproute2
 apk add wget bash curl sudo
-read -p "按回车继续执行第一个脚本..."
+read -p "按回车继续执行安装kejilion工具箱脚本..."
 clear
 curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
 
 
 # 添加回车等待
-read -p "按回车继续执行第二个脚本..."
+read -p "按回车继续执行安装hysteria2..."
 
 bash <(curl -fsSL https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/alpine-hysteria2.sh)
-read -p "按回车继续执行第三个脚本..."
+read -p "按回车继续执行安装xray..."
 bash <(curl -fsSL https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/allxray.sh)
 
