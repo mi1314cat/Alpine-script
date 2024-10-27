5ed430d7e23d1adda29cf40f8b078a19aa1f11ae
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-27 18:28:22 +0800

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -10,15 +10,25 @@ printf "\e[0m"
 apk add update
 apk add iproute2
 apk add wget bash curl sudo
-read -p "按回车继续执行安装kejilion工具箱脚本..."
-clear
-curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
-
-
+# 按回车继续执行安装kejilion工具箱脚本
+read -p "按回车继续执行安装kejilion工具箱脚本（输入n跳过）..." input
+if [[ "$input" != "n" ]]; then
+    clear
+    curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
+fi
 # 添加回车等待
-read -p "按回车继续执行安装hysteria2..."
+# 按回车继续执行安装kejilion工具箱脚本
+read -p "按回车继续执行安装alpine-hysteria2（输入n跳过）..." input
+if [[ "$input" != "n" ]]; then
+    clear
+   bash <(curl -fsSL https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/alpine-hysteria2.sh)
+fi
 
-bash <(curl -fsSL https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/alpine-hysteria2.sh)
-read -p "按回车继续执行安装xray..."
-bash <(curl -fsSL https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/allxray.sh)
+read -p "按回车继续执行安装安装xray（输入n跳过）..." input
+if [[ "$input" != "n" ]]; then
+    clear
+   bash <(curl -fsSL https://github.com/mi1314cat/Alpine-script/raw/refs/heads/main/allxray.sh)
+
+
+fi
 
