1042216e5affcb4aa5045c6bab7fd292ac023152
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-01-10 10:34:27 +0800

    Create apk-nginx.sh

---
+++ apk-nginx.sh
@@ -0,0 +1,89 @@
+nginx() {
+    # 使用 Alpine 的 apk 包管理器安装 nginx
+    apk add --no-cache nginx
+
+    # 创建 nginx 配置文件
+    cat <<EOF > /etc/nginx/nginx.conf
+user nobody;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+    gzip on;
+
+    server {
+        listen ${VMES_PORT} ssl;
+        server_name ${DOMAIN_LOWER};
+
+        ssl_certificate       "${CERT_PATH}";
+        ssl_certificate_key   "${KEY_PATH}";
+        
+        ssl_session_timeout 1d;
+        ssl_session_cache shared:MozSSL:10m;
+        ssl_session_tickets off;
+        ssl_protocols    TLSv1.2 TLSv1.3;
+        ssl_prefer_server_ciphers off;
+
+        location / {
+            proxy_pass https://pan.imcxx.com; #伪装网址
+            proxy_redirect off;
+            proxy_ssl_server_name on;
+            sub_filter_once off;
+            sub_filter "pan.imcxx.com" \$server_name;
+            proxy_set_header Host "pan.imcxx.com";
+            proxy_set_header Referer \$http_referer;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header User-Agent \$http_user_agent;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto https;
+            proxy_set_header Accept-Encoding "";
+            proxy_set_header Accept-Language "zh-CN";
+        }
+
+        location ${WS_PATH} {
+            proxy_redirect off;
+            proxy_pass http://127.0.0.1:9999;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$host;
+        }
+        location ${WS_PATH1} {
+            proxy_redirect off;
+            proxy_pass http://127.0.0.1:9998;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$host;
+        }
+        location ${WS_PATH2} {
+        proxy_request_buffering      off;
+        proxy_redirect off;
+        proxy_pass http://127.0.0.1:9997;
+        proxy_http_version 1.1;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+}
+    }
+}
+EOF
+
+    # 创建 nginx 所需的目录（如果不存在）
+    mkdir -p /run/nginx
+
+    # 启动 nginx 服务
+    rc-service nginx restart
+}
